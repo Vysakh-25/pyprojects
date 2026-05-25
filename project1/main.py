323c95a3494f254a9dc4a43fcaea561cadcf0814
Vysakh <vysakhharikumar17@gmail.com>
2026-05-25 20:50:34 +0530

    geminied

--- project1/main.py
+++ project1/main.py
@@ -1,5 +1,5 @@
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
@@ -9,8 +9,7 @@ load_dotenv()
 
 
 def main():
-    model=ChatOpenAI(temperature=0)
-
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0)
     tools=[]
 
     agent_executor = create_react_agent(model,tools)
